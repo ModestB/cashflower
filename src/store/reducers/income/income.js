a2867985f7e066904c1f552228a0fa18b250587e
Modestas Bujanauskas <modestas.bujanauskas@tglab.com>
2020-08-30 16:27:20 +0300

    add immer, remake income reducer with immer

--- src/store/reducers/income/income.js
+++ src/store/reducers/income/income.js
@@ -1,3 +1,4 @@
+import produce  from 'immer';
 import {
   INCOME_GET_ALL_REQUESTED,
   INCOME_GET_ALL_SUCCEEDED,
@@ -15,91 +16,75 @@ import {
 
 const initialState = {
   data: {},
-  types: {
-    none: { value: "", label: "None" },
-  },
+  types: {},
   incomeDataLoading: false,
   incomeAddLoading: false,
 }
 
-const addIncomeHandler = (state, payload) => {
-  const newEntry = {};
-  newEntry[payload.income.id] = payload.income
-  let updatedState = {};
-  updatedState.data = {...state.data, ...newEntry};
-  updatedState.types = {...state.types};
-  updatedState.incomeAddLoading = false;
+const requestAddIncomeHandler = (state) => {
+  const nextState = produce(state,  draftState => {
+    draftState.incomeAddLoading = true;
+  })
 
-  return updatedState
+  return nextState;
 }
 
-const deleteIncomeHandler = (state, payload) => {
-  let updatedState = {};
-  updatedState.data = {...state.data};
-  updatedState.types = {...state.types};
+const addIncomeSuccessHandler = (state, payload) => {
+  const nextState = produce(state, draftState => {
+    draftState.data[payload.income.id] = payload.income;
+    draftState.incomeAddLoading = false;
+  })
 
-  delete updatedState.data[payload.key];
+  return nextState
+}
 
-  return updatedState;
+const deleteIncomeSuccessHandler = (state, payload) => {
+  const nextState = produce(state, draftState => {
+    delete draftState.data[payload.key];
+  })
+  return nextState;
 }
 
 const requestAllIncomeData = (state) => {
-  let updatedState = {...state};
-  updatedState.incomeDataLoading = true;
+  let nextState = produce(state, draftState =>  {
+    draftState.incomeDataLoading = true;
+  })
 
-  return updatedState;
+  return nextState;
 }
 
-const setAllIncomeDataHandler = (state, payload) => {
-  let initialState = {...state};
-  initialState.incomeDataLoading = false;
-  initialState.data = {...payload.income};
-  initialState.types = {...payload.types}
-  Object.keys(initialState.data)
-    .forEach(key => initialState.data[key]['id'] = key)
-  Object.keys(initialState.types)
-    .forEach(key => initialState.types[key]['key'] = key)
-
-  return initialState
-}
-
-const requestAddIncomeHandler = (state) => {
-  let updatedState = {};
-  updatedState.data = {...state.data}
-  updatedState.incomeAddLoading = true;
-  updatedState.types = {...state.types};
-
-  return updatedState;
+const getAllIcomeDataSuccesHandler = (state, payload) => {
+  const nextState = produce(state,  draftState => {
+    draftState.incomeDataLoading = false;
+    draftState.data = {...payload.income};
+    draftState.types = {...payload.types}
+    Object.keys(draftState.data)
+      .forEach(key => draftState.data[key]['id'] = key)
+    Object.keys(draftState.types)
+      .forEach(key => draftState.types[key]['key'] = key)
+  })
+  return nextState
 }
 
 const editIncomeDataHandler = (state, payload) => {
-  let updatedState = {};
-  updatedState.data = {...state.data}
-  updatedState.data[payload.key] =  {...state.data[payload.key], ...payload.income}
-  updatedState.types = {...state.types};
-
-
-  return updatedState
+  const nextState = produce(state, draftState => {
+    draftState.data[payload.key] =  {...state.data[payload.key], ...payload.income}
+  })
+  return nextState
 }
 
 const addIncomeTypeHandler = (state, payload) => {
-  const newType = {};
-  newType[payload.key] = {...payload.type, key: payload.key};
-  const updatedState = {};
-  updatedState.data = {...state.data};
-  updatedState.types = {...state.types, ...newType};
-
-  return updatedState;
+  const nextState = produce(state, draftState  => {
+    draftState.types[payload.key] = {...payload.type, key: payload.key};
+  })
+  return nextState;
 }
 
 const deleteIncomeTypeHandler = (state, payload) => {
-  let updatedState = {};
-  updatedState.data = {...state.data};
-  updatedState.types = {...state.types};
-
-  delete updatedState.types[payload.key];
-
-  return updatedState;
+  const nextState = produce(state, draftState => {
+    delete draftState.types[payload.key];
+  })
+  return nextState;
 }
 
 export default (state = initialState, action) => {
@@ -109,7 +94,7 @@ export default (state = initialState, action) => {
     }
 
     case INCOME_GET_ALL_SUCCEEDED: {
-      return setAllIncomeDataHandler(state, action.payload);
+      return getAllIcomeDataSuccesHandler(state, action.payload);
     }
 
     case INCOME_ADD_REQUESTED: {
@@ -117,7 +102,7 @@ export default (state = initialState, action) => {
     }
 
     case INCOME_ADD_SUCCEEDED: {
-      return addIncomeHandler(state, action.payload);
+      return addIncomeSuccessHandler(state, action.payload);
     }
 
     case INCOME_EDIT_REQUESTED: {
@@ -133,7 +118,7 @@ export default (state = initialState, action) => {
     }
 
     case INCOME_DELETE_SUCCEEDED: {
-      return deleteIncomeHandler(state, action.payload);
+      return deleteIncomeSuccessHandler(state, action.payload);
     }
 
     case INCOME_TYPE_ADD_REQUESTED: {
