844063700f903d4bde54ce44b53a26c56b445ad5
Modestas Bujanauskas <modestas.bujanauskas@tglab.com>
2021-12-11 12:02:17 +0200

    Clean client

--- client/config-overrides.js
+++
@@ -1,8 +0,0 @@
-// Minimizing material-ui Bundle Size
-// https://material-ui.com/guides/minimizing-bundle-size/#option-2
-
-const { useBabelRc, override } = require('customize-cra');
-
-module.exports = override(
-  useBabelRc(),
-);
--- client/src/components/charts/partials/partials.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { ucFirst } from '../../../shared/utilities';
-
-export default function renderColorfulLegendText(value, entry) {
-  const { color } = entry;
-  return <span style={{ color }}>{ucFirst(value)}</span>;
-}
