bdcd218eb8f60b78aba753e2be547e645f149de8
Modestas Bujanauskas <modestas.bujanauskas@tglab.com>
2021-12-25 20:51:31 +0200

    Add logo

--- client/src/components/header/Header.vue
+++ client/src/components/header/Header.vue
@@ -1,33 +1,48 @@
 <template>
-  <header class="header">
-    <div class="header__title">Cashflower</div>
+  <header class="header" :class="{ 'header--notAuth': !isAuth }">
+    <transition name="logo" mode="out-in">
+      <component v-bind:is="currentLogo"></component>
+    </transition>
 
-    <base-popper :show="showMenu" :hidePopper="hideMenu">
-      <template v-slot:default>
-        <div class="header__menu__toggle" @click="toggleMenu">
-          <base-icon iconName="user" size="xs" noHover></base-icon>
-          <base-icon
-            iconName="chevron-down"
-            size="xs"
-            :rotation="arrowRotation"
-            noHover
-          ></base-icon>
-        </div>
-      </template>
-      <template v-slot:body>
-        <div class="header__menu__body">
-          <base-button type="link" @click="logoutHandler">Logout</base-button>
-          <base-button type="link">Settings</base-button>
-        </div>
-      </template>
-    </base-popper>
+    <transition name="menu">
+      <base-popper v-if="isAuth" :show="showMenu" :hidePopper="hideMenu">
+        <template v-slot:default>
+          <div class="header__menu__toggle" @click="toggleMenu">
+            <base-icon iconName="user" size="xs" noHover></base-icon>
+            <base-icon
+              iconName="chevron-down"
+              size="xs"
+              :rotation="arrowRotation"
+              noHover
+            ></base-icon>
+          </div>
+        </template>
+        <template v-slot:body>
+          <div class="header__menu__body">
+            <base-button type="link" @click="logoutHandler">Logout</base-button>
+            <base-button type="link">Settings</base-button>
+          </div>
+        </template>
+      </base-popper>
+    </transition>
   </header>
 </template>
 <script>
+import Logo from '@/components/icons/Logo.vue';
+import LogoLoggedIn from '@/components/header/LogoLoggedIn.vue';
+import LogoLoggedOut from '@/components/header/LogoLoggedOut.vue';
+
 export default {
+  components: {
+    logo: Logo,
+    'logged-in-logo': LogoLoggedIn,
+    'logged-out-logo': LogoLoggedOut,
+  },
   data() {
     return {
       showMenu: false,
+      isAuth: false, // temporaly
+      view: 'logo',
     };
   },
   methods: {
@@ -45,11 +60,14 @@ export default {
     arrowRotation() {
       return this.showMenu ? '180' : '0';
     },
+    currentLogo() {
+      return this.isAuth ? 'logged-in-logo' : 'logged-out-logo';
+    },
   },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .header {
   position: fixed;
   width: 100vw;
@@ -65,9 +83,20 @@ export default {
   background-color: var(--primary--light);
   box-shadow: var(--main-box-shadow);
 
+  &.header--notAuth {
+    justify-content: center;
+  }
+
   &__title {
     cursor: pointer;
     user-select: none;
+    display: flex;
+    align-items: center;
+
+    svg {
+      margin-right: var(--main-padding-2);
+      width: 35px;
+    }
   }
 
   &__menu__toggle {
@@ -83,5 +112,27 @@ export default {
     align-items: flex-start;
     flex-direction: column;
   }
+}
+</style>
+
+<style lang="scss" scoped>
+.logo-enter-active,
+.menu-enter-active {
+  transition: all 0.5s ease;
+}
+
+.logo-leave-active {
+  transition: all 0.5s ease-out;
+}
+
+.logo-enter-from,
+.logo-leave-to {
+  opacity: 0;
+}
+
+.menu-leave-from,
+.menu-leave,
+.menu-enter-from {
+  opacity: 0;
 }
 </style>
---
+++ client/src/components/header/LogoLoggedOut.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="header__title"><logo></logo>Cashflower</div>
+</template>
+<script>
+import Logo from '@/components/icons/Logo.vue';
+
+export default {
+  components: {
+    logo: Logo,
+  },
+};
+</script>
